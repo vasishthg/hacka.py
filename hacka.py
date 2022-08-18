1bb7cf4fe489244ade65de3ab5c5fc63396c8ce8
Vasishth Gupta <vasishthemail@gmail.com>
2022-08-18 21:34:00 +0530

    e

---
+++ hacka.py
@@ -0,0 +1,11 @@
+import re
+from flask import Flask, render_template, request, session, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
